9f24a9c38aab8bc58f6b0af1087f3447e0c2ccaf
Nikhil <nsnikhil1707@gmail.com>
2022-07-01 12:53:57 +0530

    New Files Added

---
+++ First.py
@@ -0,0 +1,23 @@
+# Mylist=["Nikhil","Pooja","Akash","Arun"]
+# Mylist.append("tarun")
+# del Mylist[1]
+# print(Mylist)
+
+# mtuple=("Student","Teachers","Principles","Clerk","Watchman")
+# l1=list(mtuple)
+# l1.pop(0)
+# tupl=tuple(l1)
+# print(f"This Tuple looks like {tupl}")
+
+# myclass=[["Nikhil"],["Arun"],["Ram"]]
+# print(myclass[0][0])
+# from re import I
+
+
+# print(*range(0, 23,2))
+
+i=1
+while (i<=10) :
+    print(i)
+    i+=1
+    print("i is at ",i)
